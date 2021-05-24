4564b2988696d08cb0f0f01802c2aa98c9d1a7e1
PaulMouchel <paulmouchel@live.fr>
2021-05-24 10:16:01 +0200

    ready to start create article page

--- src/pages/NewArticle.jsx
+++ src/pages/NewArticle.jsx
@@ -1,39 +1,22 @@
 import React, { useState } from 'react';
-import ProgressBar from '../components/ProgressBar';
+import Title from '../components/Title';
+import UploadForm from '../components/UploadForm';
+import ImageGrid from '../components/ImageGrid';
+import Modal from '../components/Modal';
 
 import './test.css'
 
 const NewArticle = () => {
-  const [file, setFile] = useState(null);
-  const [error, setError] = useState(null);
-
-  const types = ['image/png', 'image/jpeg'];
-
-  const handleChange = (e) => {
-    let selected = e.target.files[0];
-
-    if (selected && types.includes(selected.type)) {
-      setFile(selected);
-      setError('');
-    } else {
-      setFile(null);
-      setError('Please select an image file (png or jpg)');
-    }
-  };
+  const [selectedImg, setSelectedImg] = useState(null);
 
   return (
     <div>
-      <form>
-        <label>
-          <input type="file" onChange={handleChange} />
-          <span>+</span>
-        </label>
-        <div className="output">
-          { error && <div className="error">{ error }</div>}
-          { file && <div>{ file.name }</div> }
-          { file && <ProgressBar file={file} setFile={setFile} /> }
-        </div>
-      </form>
+      <Title/>
+      <UploadForm />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      { selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 }
