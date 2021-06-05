edf54c41fd571147984e064af2a9ff16a9e09c2d
PaulMouchel <paulmouchel@live.fr>
2021-06-05 14:48:01 +0200

    add executive or not in member display

--- src/components/MemberDescription.jsx
+++ src/components/MemberDescription.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const MemberDescription = ({fullName, role, holder, president, even}) => {
+const MemberDescription = ({fullName, role, holder, executive, president, even}) => {
   return (
         <div className={`flex flex-col justify-center align-center  p-2  ${even ? "text-right" : ""}`}>
                 <h3 className="text-gray-700 text-xl md:text-2xl font-bold">{fullName}</h3>
-                {!president && <p className="text-gray-700 text-xl">{ holder ? "Titulaire" : "Suppléant"}</p>}
+                {!president && <>
+                  <p className="text-gray-700 text-xl">Collège {!executive && "non "}cadre</p>
+                  <p className="text-gray-700 text-xl">{ holder ? "Titulaire" : "Suppléant"}</p>
+                </>}
                 <p className="text-gray-700 text-lg">{role}</p>
         </div>
     );
