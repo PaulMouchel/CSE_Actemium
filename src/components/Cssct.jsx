003ad6d424b4ee2e6fa8abd8ae0ba96cd7ea8d97
PaulMouchel <paulmouchel@live.fr>
2021-06-08 12:10:13 +0200

    Change CSSCT design

--- src/components/Cssct.jsx
+++ src/components/Cssct.jsx
@@ -5,16 +5,19 @@ import CssctMission from './CssctMission.jsx'
 
 const Cssct = ({admin, textColor}) => {
   const { docs } = useFirestore('Cssct');
-
+  
   return (
     <div className="min-h-screen pb-10">
       <Title id="cssct" textColor={textColor}>CSSCT</Title>
       <div className="pb-4">
         <p className="pb-6 text-xl text-center text-gray-50">Les missions de la CSSCT</p>
-        <div>
+        <p className="h-0 md:invisible text-center text-gray-50">(Cliquer sur une image pour afficher le détail)</p>
+        <div className="flex w-full h-screen items-center justify-center">
+          <div className="h-sreen flex w-5/6 h-5/6 flex-col md:flex-row">
           {docs && docs.map((mission, index) =>
-                <CssctMission key={index} {...mission} admin={admin} even={index%2 === 0} last={index===docs.length - 1} textColor={textColor}/>
+              <CssctMission key={index} {...mission} admin={admin}/>
           )}
+          </div>
         </div>
       </div>
     </div>
--- src/components/CssctMission.jsx
+++ src/components/CssctMission.jsx
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFirestore from '../hooks/useFirestore';
 import deleteDocument from '../hooks/deleteDocument';
 import DeleteButton from './DeleteButton.jsx'
+import { motion } from 'framer-motion'
 
-const CssctMission = ({title, text, imageUrl, even, admin, id, last, textColor}) => {
+const CssctMission = ({title, text, imageUrl, admin, id}) => {
   
   const { docs } = useFirestore('Cssct');
+  const [size, setSize] = useState(1)
+  const [showText, setShowText] = useState(false)
 
   const handleDelete = () => {
     deleteDocument({docs, id, collection:'Cssct'})
   }
 
-  return (
-    <div className={`py-4 md:flex justify-between md:flex-row${!even && "-reverse"} border-gray-500 ${!last && "border-b"}`}>
-      <div className="md:w-1/2 md:px-20 text-center flex flex-col justify-center pb-4 md:pb-0">
-        <p className={`text-${textColor} text-2xl font-bold pb-2`}>{title}</p>
-        <p className={`text-${textColor} text-xl`}>{text}</p>
-      </div>
+  const handleMouseEnter = () => {
+    let elements = docs.length
+    setSize(elements+1)
+    setShowText(true)
+  }
 
-      <div className="md:w-1/2 md:px-4 flex justify-center md:mx-4">
-        <div className="h-80 md:h-96 w-full bg-cover bg-center" style={{backgroundImage: `url(${imageUrl})`}}>
+  const handleMouseLeave = () => {
+    setSize(1)
+    setShowText(false)
+  }
+
+  return (
+    <>
+      <div 
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave} 
+        className="slide relative flex-auto hover:flex-5 bg-cover bg-center transition-all duration-500 ease-in-out hover:flex-grow" 
+        style={{backgroundImage:`url(${imageUrl})`, flex: size}}>
+        { showText && 
+          <motion.div className="bg-gradient-to-t from-gray-900 h-full px-4 text-start flex flex-col justify-between items-start py-4"
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          transition={{delay: 0.4, duration: 0.7}}>
             <DeleteButton admin={admin} onClick={handleDelete}/>
-        </div>
+            {!admin && <div/>}
+            <div>
+            <p className={`text-gray-50 text-2xl font-bold mt-8 pb-2`}>{title}</p>
+            <p className={`text-gray-50 text-xl`}>{text}</p>
+            </div>  
+          </motion.div>
+        }
       </div>
-    </div>
+    </>
   );
 }
 
