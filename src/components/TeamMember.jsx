fb33f771c94d8bd9a6db1f663b2d9379a2375973
PaulMouchel <paulmouchel@live.fr>
2021-06-16 21:13:16 +0200

    fix display issue on mobile for team section

--- src/components/TeamMember.jsx
+++ src/components/TeamMember.jsx
@@ -16,11 +16,11 @@ const TeamMember = ({imageUrl, fullName, role, holder, executive, president, las
 
   return (
         <>
-            <div className={`w-full flex ${!even && "flex-row-reverse"} justify-center max-h-40 md:max-h-56`}>
+            <div className={`w-full flex flex-row${!even && "-reverse"} justify-center md:max-h-56`}>
                 <div className={`md:w-1/4 flex justify-${even ? "end" : "start"}`}>
                     <MemberDescription even={even} fullName={fullName} role={role} holder={holder} executive={executive} president={president} />
                 </div>
-                <div className="w-40 md:w-56 flex justify-center mx-4">
+                <div className="w-40 md:w-56 flex items-center justify-center mx-4">
                     <div className="h-40 md:h-56 w-40 md:w-56 bg-cover bg-center rounded-full border-8" style={{backgroundImage: `url(${imageUrl})`}}>
                         <DeleteButton admin={admin} onClick={handleDelete}/>
                     </div>
