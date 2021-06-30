c8d3dc6bcd3c8de9bb28b14b3fb349d7953f549e
PaulMouchel <paulmouchel@live.fr>
2021-06-30 17:35:09 +0200

    adjust CSSCT to screen size

--- src/components/Cssct.jsx
+++ src/components/Cssct.jsx
@@ -7,13 +7,13 @@ const Cssct = ({admin, textColor}) => {
   const { docs } = useFirestore('Cssct');
   
   return (
-    <div className="min-h-screen pb-10">
+    <div className="h-screen pb-10">
       <Title textColor={textColor}>CSSCT</Title>
-      <div className="pb-4 h-screen">
+      <div className="pb-4 h-4/6">
         <p className="text-xl text-center text-gray-50">Les missions de la CSSCT</p>
         <p className="h-0 md:invisible text-center text-gray-50">(Cliquer sur une image pour afficher le détail)</p>
-        <div className="flex w-full h-full items-center justify-center">
-          <div className="flex w-5/6 h-4/6 flex-col md:flex-row">
+        <div className="flex w-full h-full my-8 items-center justify-center">
+          <div className="flex w-5/6 h-full flex-col md:flex-row">
           {docs && docs.map((mission, index) =>
               <CssctMission key={index} {...mission} admin={admin}/>
           )}
