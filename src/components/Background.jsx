a7016b6bc3514d2c49aa8d71ec0cffef94898ed6
PaulMouchel <paulmouchel@live.fr>
2021-08-19 06:49:55 +0200

    fix backgroud issue

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,20 +4,22 @@ import useFirestore from '../hooks/useFirestore';
 const Background = (props) => {
     
     const { docs } = useFirestore('Background');
-    const [diplayedImage, setDisplayedImage] = useState("")
+    const [displayedImage, setDisplayedImage] = useState("")
 
     useEffect(() => {
         if (props.image) {
             setDisplayedImage(props.image)
         } else if (docs[0]) {
-            setDisplayedImage(docs[0].imageUrl)
-        } else
-        setDisplayedImage("")
+            //setDisplayedImage(docs[0].imageUrl)
+            props.setImage(docs[0].imageUrl)
+        } else {
+            setDisplayedImage("")
+        }
         },[props.image, docs]);
 
     return (
         <div className={`w-screen min-h-screen bg-cover bg-center bg-fixed ${props.className}`}
-            style={docs[0] && {backgroundImage: `url(${diplayedImage})`}}>
+            style={docs[0] && {backgroundImage: `url(${displayedImage})`}}>
                 {props.children}
         </div>
     );
