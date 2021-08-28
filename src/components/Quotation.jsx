486982c66bc42382ea34a7424834ed5c864e0e09
PaulMouchel <paulmouchel@live.fr>
2021-08-28 14:37:38 +0400

    Quotation display with loader

--- src/components/Quotation.jsx
+++ src/components/Quotation.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
+import { faQuoteLeft, faQuoteRight, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useFirestore from '../hooks/useFirestore';
 import { motion } from 'framer-motion'
@@ -9,30 +9,37 @@ const Quotation = () => {
     const { docs } = useFirestore('Quotation');
 
     return (
-        <>
-            {docs[0] && docs[0].text !== "" ?
-                <motion.div className="bg-gray-900 bg-opacity-70 max-h-96 mx-8 md:mx-48 lg:mx-64"
-                    initial={{opacity:0}}
-                    animate={{opacity:1}}
-                    transition={{delay:0.6}}>
-                    <div className="text-gray-50 p-4 md:p-6 lg:p-10 italic flex justify-between rounded-lg text-2xl text-justify">
-                        <div className="flex items-start">
-                            <FontAwesomeIcon icon={faQuoteLeft}/>
-                        </div>
-                    
-                        <div className="overflow-hidden">
-                            <blockquote className="pb-2 px-4 md-px-6 lg:px-10">{docs[0].text}</blockquote> 
-                            {docs[0].author && <blockquote className="text-xl text-right px-10">- {docs[0].author}</blockquote>}
-                        </div>
-                            
-                        <div className="flex items-end">
-                            <FontAwesomeIcon icon={faQuoteRight}/>
-                        </div>
-                    </div>
-                </motion.div> : <div/>
-            }
+        <motion.div className="bg-gray-900 bg-opacity-70 max-h-96 mx-8 md:mx-48 lg:mx-64"
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{delay:0.6}}>
 
-        </>
+            <div className="text-gray-50 p-4 md:p-6 lg:p-10 italic flex justify-between rounded-lg text-2xl text-justify">
+                <div className="flex items-start">
+                    <FontAwesomeIcon icon={faQuoteLeft}/>
+                </div>
+                
+                <div className="overflow-hidden">
+                { docs[0] ?
+                    <>
+                        <blockquote className="pb-2 px-4 md-px-6 lg:px-10">{docs[0].text}</blockquote> 
+                        {docs[0].author && <blockquote className="text-xl text-right px-10">- {docs[0].author}</blockquote>}
+                    </>
+                    :
+                    <> 
+                        <blockquote className="pb-2 px-4 md-px-6 lg:px-10 text-center">
+                            <FontAwesomeIcon className="animate-spin" icon={faSpinner}/>
+                        </blockquote> 
+                    </>
+                }
+                </div>
+                
+                <div className="flex items-end">
+                    <FontAwesomeIcon icon={faQuoteRight}/>
+                </div>
+            </div>
+        </motion.div>
+
     );
 }
 
