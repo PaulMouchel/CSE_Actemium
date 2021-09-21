ffded684d00e224ca4a4480f93c1771dc106a1d1
PaulMouchel <paulmouchel@live.fr>
2021-09-21 09:41:45 +0400

    swipe on mobile device to navigate through pics

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,6 +3,10 @@ import { motion } from 'framer-motion';
 
 const Modal = ({ setSelectedImg, selectedImg, galleryUrl }) => {
 
+  let xDown = null;                                                        
+  let yDown = null;
+
+  // Navigation avec les flèches du clavier, contrôle de la touche
   const keyCheck = (e) => {
     switch (e.keyCode) {   
         case 37:
@@ -16,12 +20,57 @@ const Modal = ({ setSelectedImg, selectedImg, galleryUrl }) => {
             break;
         default:
             break;
-    }}
+    }
+  }
+
+  // Navigation en swippant sur mobile : pose du doigt
+  const handleTouchStart = (e) => {
+    const firstTouch = e.touches[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+  };                                                
+  
+  // Navigation en swippant sur mobile : déplacement du doigt
+  const handleTouchMove = (e) => {
+      if ( ! xDown || ! yDown ) {
+          return;
+      }
+  
+      const xUp = e.touches[0].clientX;                                    
+      const yUp = e.touches[0].clientY;
+  
+      const xDiff = xDown - xUp;
+      const yDiff = yDown - yUp;
+                                                                           
+      if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+          if ( xDiff > 0 ) {
+              /* right swipe */ 
+              navigate(e, 1)
+          } else {
+              /* left swipe */
+              navigate(e, -1)
+          }                       
+      } 
+      else {
+          if ( yDiff > 0 ) {
+              /* down swipe */ 
+          } else { 
+              /* up swipe */
+          }                                                                 
+      }
+      /* reset values */
+      xDown = null;
+      yDown = null;                                             
+  };
 
   useEffect(() => {
     document.addEventListener('keydown', keyCheck);
+    document.addEventListener('touchstart', handleTouchStart, false);        
+    document.addEventListener('touchmove', handleTouchMove, false);
     return () => {
       document.removeEventListener('keydown', keyCheck)
+      document.removeEventListener('touchstart', handleTouchStart, false);        
+      document.removeEventListener('touchmove', handleTouchMove, false);
     }
   }, [keyCheck]);
 
@@ -46,7 +95,7 @@ const Modal = ({ setSelectedImg, selectedImg, galleryUrl }) => {
       animate={{ opacity: 1 }}
       exit={{opacity: 0}}
     >
-      <div className="fixed left-0 w-20 h-20 flex items-center justify-center z-50" onClick={e => navigate(e, -1)}> 
+      <div className="fixed left-0 w-20 h-20 hidden md:flex items-center justify-center z-50" onClick={e => navigate(e, -1)}> 
         <div className="bg-gray-300 w-10 h-2 rounded-full transform -rotate-45 absolute -translate-y-3"/>
         <div className="bg-gray-300 w-10 h-2 rounded-full transform rotate-45 absolute translate-y-3"/>
       </div>
@@ -59,7 +108,7 @@ const Modal = ({ setSelectedImg, selectedImg, galleryUrl }) => {
         initial={{ y: "-100vh" }}
         animate={{ y: 0 }}
       />
-      <div className="fixed right-0 w-20 h-20 flex items-center justify-center z-50" onClick={e => navigate(e, 1)}> 
+      <div className="fixed right-0 w-20 h-20 hidden md:flex items-center justify-center z-50" onClick={e => navigate(e, 1)}> 
         <div className="bg-gray-300 w-10 h-2 rounded-full transform rotate-45 absolute -translate-y-3"/>
         <div className="bg-gray-300 w-10 h-2 rounded-full transform -rotate-45 absolute translate-y-3"/>
       </div>
