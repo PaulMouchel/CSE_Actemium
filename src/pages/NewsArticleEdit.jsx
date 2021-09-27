7a16d8e94ad8bdf7a82a6402eed24d66852ee873
PaulMouchel <paulmouchel@live.fr>
2021-09-27 13:49:23 +0400

    fix date issue

--- src/pages/NewsArticleEdit.jsx
+++ src/pages/NewsArticleEdit.jsx
@@ -29,7 +29,7 @@ const NewsArticleEdit = ({collection}) => {
     
     const currentTime = new Date()
     // returns the month (from 0 to 11)
-    const month = ('0' + currentTime.getMonth() + 1).slice(-2)
+    const month = ('0' + (currentTime.getMonth() + 1)).slice(-2)
     // returns the day of the month (from 1 to 31)
     const day = ('0' + currentTime.getDate()).slice(-2)
     // returns the year (four digits)
