63bf0537f61573f177f23766e1d44b3d846dd9d6
PaulMouchel <paulmouchel@live.fr>
2024-01-24 17:00:23 +0400

    fix contact email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = ({textColor}) => {
         <div className="pb-10">
             <Title textColor={textColor}>Nous contacter</Title>
             <p className="text-center pb-4 text-lg">Toute idée est la bienvenue !</p>
-            <a href = "mailto:ce.actemium.rennes@gmail.com" className='w-full mx-auto flex justify-center my-24'>
+            <a href={`mailto:${import.meta.env.VITE_CONTACT_EMAIL}`} className='w-full mx-auto flex justify-center my-24'>
                 <ActionButton className="mt-2 w-full md:w-80 self-end p-6 text-xl" type="button">
                     <FaEnvelope className="text-white mr-2 inline-block"/>
                     Envoyer un email au CSE
